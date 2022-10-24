3a1659f366d6448b3b43905ce49e3a9f7de1ac23
Huiqing Sun <adsunhq@gmail.com>
2022-10-24 17:26:11 -0400

    project final

---
+++ code/01_make_table1.R
@@ -0,0 +1,29 @@
+here::i_am(
+  "code/01_make_table1.R"
+)
+
+
+data <- readRDS(
+  file = here::here("data/data_clean.rds")
+)
+
+library(expss)
+library(labelled)
+library(gtsummary)
+
+library(plyr)
+
+
+
+table_one <- data %>%
+  select("Grade","Race","Marital.Status","Estrogen.Status","Status") %>%
+  tbl_summary(by = Status) %>%
+  modify_spanning_header(c("stat_1", "stat_2") ~ "**Summary**") %>%
+  add_overall() %>%
+  add_p()
+
+
+saveRDS(
+  table_one,
+  file = here::here("output/table_one.rds")
+)
